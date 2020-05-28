5b10ebe15b8486ce759a262cd75762b4f9fd4f07
Adrian Azucena <azucenaadrian00@gmail.com>
2020-05-29 02:55:03 +0800

    all in registration

--- backend/backsite/verification.php
+++ backend/backsite/verification.php
@@ -10,9 +10,9 @@ include_once("../api/database.php");
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
 
- $db=new database();
- $connection=$db->connect();
- $user=new Users($connection);
+ $db=new database();    //Create another database object
+ $connection=$db->connect();    //Create a variable for connection
+ $user=new Users($connection);    //Initialize created connection to the sql api
 
  if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -28,7 +28,6 @@ include_once("../api/database.php");
       $user->country= $user->conn->real_escape_string(trim($request->country));
       $user->email= $user->conn->real_escape_string(trim($request->email));
       $user->password= $user->conn->real_escape_string(trim($request->password));
-       //print_r($generated_code);
        if($user->register_account()){
          echo json_encode(array(
            "status" =>1,
