3994ef99757ea55f83ce7530cfa52106fed9aed0
ysa001 <aklelesterio@gmail.com>
2020-05-31 09:50:09 +0800

    fixed register link as per Adrian's request and set maxlength of password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
-    path: '', component:LoginComponent
+    path: 'login', component:LoginComponent
   },
   {
-    path: 'home', component:HomeComponent
+    path: '', component:HomeComponent
   }  
 ];
 
